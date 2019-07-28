036c5cedb259cebf90a295ccfbfe10725b84fb89
AsyncVoid <cookieassassin32@gmail.com>
2019-07-28 16:09:12 +0100

    change email error

--- js/contact_us.js
+++ js/contact_us.js
@@ -65,7 +65,7 @@ $(function() {
     {
         $('#success').html("<div class='alert alert-danger'>");
         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-        $('#success > .alert-danger').append($("<strong>").text("Sorry, it seems that my mail server is not responding. Please try again later!"));
+        $('#success > .alert-danger').append($("<strong>").text("Sorry, it seems that my mail server is not responding. Please try and contact us via contact@devhead.net directly!"));
         $('#success > .alert-danger').append('</div>');
         $('#contactForm').trigger("reset");
     }
